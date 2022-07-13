646b9fc57efbe5a7aa6dd896acdee2f7efc5d0be
Iwan Firmawan <firmawaneiwan@gmail.com>
2022-07-13 21:51:08 +0700

    Create FCFS class

---
+++ src/cloudsimproject/CloudsimProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cloudsimproject;
+
+
+/**
+ *
+ * @author iwanfirmawan
+ */
+public class CloudsimProject {
+    public static void main(String[] args) {
+        FCFS_Scheduler cek = new FCFS_Scheduler();
+    }
+}
---
+++ src/utils/Constants.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author iwanfirmawan
+ */
+public class Constants {
+    public static final int NO_OF_TASKS = 30; // number of Cloudlets;
+    public static final int NO_OF_DATA_CENTERS = 5; // number of Datacenters;
+    public static final int POPULATION_SIZE = 25; // Number of Particles.
+}
